37d11676fae0c8704af633375c0ca1ef2beb9d3b
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2021-06-04 02:29:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Racines Maliens</title>
+</head>
+<body>
+    <h1>Racines Malines</h1>
+</body>
+</html>
