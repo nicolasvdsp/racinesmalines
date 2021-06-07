aab8d33d465feb1b7b4f3e56795a4b7ec44c07a3
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2021-06-07 02:48:35 +0200

    screen[home]: adds homescreen and navigationbar

--- index.php
+++ index.php
@@ -38,8 +38,27 @@
 
     <h2 style="margin-top: 100px;">Index</h2>
    
-   
+    <div class="cardview" style="padding-bottom: 24px;">
+        <h3 class="btnSpaceBefore">Navigeer naar de moestuin</h3>
+        <a href="https://www.google.be" class="btn btn--green btn--margin maps">Navigeer met Maps</a>
+    </div>
+    <a class="cardview cardview--connect btn--max"><h3 class="white alignC">Maak kennis met leden uit je moestuin</h3></a>
+    <div class="cardview">
+        <h3 class="btnSpaceBefore">Open de poort van de moestuin</h3>
+        <a href="#" class="btn btn--green btn--margin lock">Ontgrendel</a>
+        <p class="info" style="margin-bottom: 0px;">Momenteel op slot</p>
+    </div>
+    <div class="cardview">
+        <h3>Kies zaden die je gaat planten</h3>
+        <p class="info" style="margin-bottom: 0px;">Als je deze informatie invult krijg je betere suggesties in de app</p>
+    </div>
 
+    <nav class="navbar">
+        <a href="index.php" class="navbar__btn">Home</a>
+        <a href="tips.php" class="navbar__btn">Tips</a>
+        <a href="chat.php" class="navbar__btn">Chats</a>
+        <a href="settings.php" class="navbar__btn">Instellingen</a>
+    </nav>
 
 </div>
 </section>
